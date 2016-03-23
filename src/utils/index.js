bb31ecb09cc849fed5a3c642add5df723886a6ef
Alec Zopf <alec.zopf@gmail.com>
2016-03-23 14:43:07 -0400

    ready for depoy: disallowing mutation of all fields starting with __, plus "pre" middleware args restructuring

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ import Middleware from './Middleware';
 function addHooks(resolver, {pre, post} = {}) {
   return async function resolve(...args) {
     const preMiddleware = new Middleware(pre);
-    await preMiddleware.compose(...args);
+    args = await preMiddleware.compose(...args) || args;
     const postMiddleware = new Middleware(post);
     const result = await resolver(...args);
     return await postMiddleware.compose(result, ...args) || result;
